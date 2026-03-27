e330af9dbefe408885337f586dc49ac4cd0f5a7c
Christian Hammond <christian@beanbaginc.com>
2026-03-27 01:06:42 -0700

    Release buildthings 1.0.

---
+++ NEWS.rst
@@ -0,0 +1,7 @@
+====================
+Buildthings Releases
+====================
+
+Buildthings 1.0 (27-March-2026):
+
+* Initial public release.
---
+++ buildthings/__init__.py
@@ -0,0 +1,22 @@
+"""Buildthings Python build backend.
+
+Version Added:
+    1.0
+"""
+
+from buildthings._version import (VERSION,
+                                  __version__,
+                                  __version_info__,
+                                  get_package_version,
+                                  get_version_string,
+                                  is_release)
+
+
+__all__ = [
+    'VERSION',
+    '__version__',
+    '__version_info__',
+    'get_package_version',
+    'get_version_string',
+    'is_release',
+]
